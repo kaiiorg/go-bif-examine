bd8c551d40957d7a6fb6c38db82c874b74c58df7
kaiiorg <k349@live.com>
2023-12-22 19:07:01 -0600

    Replace waitgroup and error channel with errgroup

--- go.mod
+++ go.mod
@@ -47,6 +47,7 @@ require (
 	github.com/zclconf/go-cty v1.13.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
--- pkg/repositories/examine_repository/resource.go
+++ pkg/repositories/examine_repository/resource.go
@@ -1,9 +1,8 @@
 package examine_repository
 
 import (
-	"errors"
+	"golang.org/x/sync/errgroup"
 	"gorm.io/gorm"
-	"sync"
 	"time"
 
 	"github.com/kaiiorg/go-bif-examine/pkg/models"
@@ -11,36 +10,32 @@ import (
 
 func (r *GormExamineRepository) CreateManyResources(resources []*models.Resource) error {
 	batchSize := 100
+	errGroup := errgroup.Group{}
 	resourcesChan := make(chan *models.Resource, 5)
 	batchChan := make(chan []*models.Resource, 5)
-	errChan := make(chan error, 5)
-	wg := &sync.WaitGroup{}
 
-	go r.batchResources(batchSize, resourcesChan, batchChan, wg)
-	go r.insertBatches(batchChan, errChan, wg)
-
-	// Loop through the resources, make sure their BifID and ProjectID are set, then pass them onto the batcher go routine
-	for _, resource := range resources {
-		resource.BifID = resource.Bif.ID
-		resource.ProjectID = resource.Project.ID
-		resourcesChan <- resource
-	}
-	close(resourcesChan)
-
-	errCount := 0
-	var finalErr error
-
-	for err := range errChan {
-		// Don't add more than 5 errors to the finalErr
-		if errCount > 5 {
-			continue
+	errGroup.Go(func() error {
+		return r.batchResources(batchSize, resourcesChan, batchChan)
+	})
+	errGroup.Go(func() error {
+		return r.insertBatches(batchChan)
+	})
+	errGroup.Go(func() error {
+		// Loop through the resources, make sure their BifID and ProjectID are set, then pass them onto the batcher go routine
+		for _, resource := range resources {
+			resource.BifID = resource.Bif.ID
+			resource.ProjectID = resource.Project.ID
+			resourcesChan <- resource
 		}
-		finalErr = errors.Join(finalErr, err)
-		errCount++
+		close(resourcesChan)
+		return nil
+	})
+
+	if err := errGroup.Wait(); err != nil {
+		return err
 	}
 
-	wg.Wait()
-	return finalErr
+	return nil
 }
 
 func (r *GormExamineRepository) FindProjectResourcesForBif(projectId uint, bifId uint) ([]*models.Resource, error) {
@@ -71,7 +66,7 @@ func (r *GormExamineRepository) GetResourceForWhisper() (*models.Resource, error
 	// TODO find a better way of doing this; there's a small chance that we could query for a record,
 	// then while we're about to update the job started column, another request grabs the same record
 	err := r.db.
-		Where("deleted_at IS NULL AND offset_to_data != 0 AND size != 0 AND job_duration IS NULL").
+		Where("deleted_at IS NULL AND offset_to_data != 0 AND size != 0 AND job_duration = ''").
 		Order("job_started").
 		Limit(1).
 		First(resource).Error
